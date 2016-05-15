91b386b4de2285c3ba861fc9f91e2cfab924659e
Kevin Jones <kevin@vcsjones.com>
2016-05-15 09:08:30 -0400

    Fixing sync path.

--- _deploy/reload.sh
+++ _deploy/reload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-rsync -r --delete --existing --ignore-non-existing "$DIR" "/var/wwwroot/vcsjones.com/"
+rsync -r --delete --existing --ignore-non-existing "$DIR/../_site" "/var/wwwroot/vcsjones.com/"
 service nginx reload
