bf96642cfb7203f88e59d7cc6c470ff960466419
Kevin Jones <kevin@vcsjones.com>
2016-05-15 09:19:52 -0400

    Adding verbosity for logs.

--- _deploy/reload.sh
+++ _deploy/reload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-rsync -r --delete --existing --ignore-non-existing "$DIR/../_site/" "/var/wwwroot/vcsjones.com/"
+rsync --verbose -r --delete --existing --ignore-non-existing "$DIR/../_site/" "/var/wwwroot/vcsjones.com/"
 service nginx reload
