de012e14a8d2b63fd28432f1e94c7b7ea96d3065
Kevin Jones <kevin@vcsjones.com>
2016-05-15 08:58:23 -0400

    Don't empty the whole directory during publish since that causes temporary 403s.

--- _deploy/publish.sh
+++ _deploy/publish.sh
@@ -3,4 +3,3 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $DIR/..
 npm install && gulp
 popd
-rm -rf /var/wwwroot/vcsjones.com/*
--- _deploy/reload.sh
+++ _deploy/reload.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+rsync -r --delete --existing --ignore-non-existing "$DIR" "/var/wwwroot/vcsjones.com/"
 service nginx reload
