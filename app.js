487d93d4c9cfc35c76dae86acd0245b4538fe0d1
Kevin Jones <kj@upside.com>
2016-09-01 21:51:14 -0400

    Fixing "sandbox" issue with CSP and SRI.

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var static = require('node-static');
 
-var file = new static.Server('./_site', {cache: 0});
+var headers = {
+    'Referrer-Policy': 'no-referrer',
+    'X-XSS-Protection': '"1; mode=block"',
+    'X-Content-Type-Options': 'nosniff',
+    'Content-Security-Policy': `"default-src 'none' ; style-src 'self' ; img-src 'self' ; frame-ancestors 'none' ; form-action 'none' ; block-all-mixed-content; reflected-xss block; referrer no-referrer"`,
+    'X-Frame-Options': 'DENY'
+};
+var file = new static.Server('./_site', {cache: 0, headers : headers});
  
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
