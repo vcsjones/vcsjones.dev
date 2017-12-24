cb8e7f75419e18d6923cc37e22a553a2bac04ce7
Kevin Jones <kevin@vcsjones.com>
2017-12-24 17:14:01 -0500

    Reload caddy on deploy.

--- _deploy/reload.sh
+++ _deploy/reload.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rsync --verbose -r --delete --existing --ignore-non-existing "$DIR/../_site/" "/var/wwwroot/vcsjones.com/"
+systemctl reload caddy.service
