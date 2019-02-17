59add4d914bfaa4f86383fc6d21204a1acb83d29
Kevin Jones <kevin@vcsjones.com>
2019-02-17 15:20:55 -0500

    Include more things for code deploy.

--- _deploy/publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source /etc/profile.d/rvm.sh
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pushd $DIR/..
-gem install bundler --no-doc
-bundle install
-yarn install && gulp
-popd
