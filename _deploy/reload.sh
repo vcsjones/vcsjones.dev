f9dbcbaf73a0ff7bbdecf77c3b75bfd7f231690f
Kevin Jones <kevin@vcsjones.com>
2024-04-22 11:57:18 -0400

    Remove AWS goo

--- _deploy/reload.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-rsync --verbose -r --delete --existing --ignore-non-existing "$DIR/../_site/" "/var/wwwroot/vcsjones.com/"
-systemctl reload caddy.service
